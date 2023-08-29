cf522964c299936730295d5c2364c07e6f2ed1ed
Kay <info@beckmann-md.de>
2023-08-29 20:47:02 +0200

    added material button modul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,9 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatIconModule } from "@angular/material/icon";
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserComponent } from './user/user.component';
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { UserComponent } from "./user/user.component";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent, UserComponent],
@@ -18,7 +19,8 @@ import { UserComponent } from './user/user.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
