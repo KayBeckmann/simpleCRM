a97c1ec73448fcc46e03a5f267f49d3233508512
Kay <info@beckmann-md.de>
2023-09-07 18:47:05 +0200

    two-way-binding input-field variable 'user'

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,8 +1,21 @@
 import { Component } from "@angular/core";
+import { User } from "src/models/user.class";
 
 @Component({
   selector: "app-dialog-add-user",
   templateUrl: "./dialog-add-user.component.html",
   styleUrls: ["./dialog-add-user.component.scss"]
 })
-export class DialogAddUserComponent {}
+export class DialogAddUserComponent {
+  user: User = new User();
+  birthDate!: Date;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
+    console.log(this.user);
+  }
+}
