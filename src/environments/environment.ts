9d78dcaffc5216d4eb1941cd9bef03fc689537d1
Kay <info@beckmann-md.de>
2023-09-09 18:33:25 +0200

    added firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kay-simplecrm',
+    appId: '1:364589877506:web:0301393ca82f7f65fc9a1b',
+    storageBucket: 'kay-simplecrm.appspot.com',
+    apiKey: 'AIzaSyDBTgbHCgefEiHvkvKar7Lr1HBSi0KvC9Q',
+    authDomain: 'kay-simplecrm.firebaseapp.com',
+    messagingSenderId: '364589877506',
+  },
+};
